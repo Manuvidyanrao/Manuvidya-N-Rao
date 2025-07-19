820dd71d18db0032ea838ff0ca103d60d9bd6302
Manuvidyanrao <153750211+Manuvidyanrao@users.noreply.github.com>
2025-07-19 20:37:48 +0530

    Create Program4.java

---
+++ Program4.java
@@ -0,0 +1,20 @@
+// Program4.java
+import java.util.*;
+
+public class Program4 {
+    public static void main(String[] args) {
+        int[] input = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        Map<Integer, Integer> countMap = new LinkedHashMap<>();
+
+        for (int i = 1; i <= 9; i++) {
+            int count = 0;
+            for (int num : input) {
+                if (num % i == 0)
+                    count++;
+            }
+            countMap.put(i, count);
+        }
+
+        System.out.println(countMap);
+    }
+}
